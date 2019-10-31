14fbf9de84b071e6202142ebb783bfc3f3f2bda8
Marcin Polak <mapoart@gmail.com>
2019-10-31 14:19:09 +0100

    init

---
+++ templates/default.go.js
@@ -0,0 +1,9 @@
+// additional info for templates like copy extra libraries.
+const config = {
+  files: [],
+  commands: ["go install github.com/json-iterator/go"],
+  repos: [],
+  descriptions: []
+};
+
+module.exports = config;
