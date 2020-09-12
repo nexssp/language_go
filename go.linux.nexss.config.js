0a96697e0faf18b78bb455fa291d32484359dbe5
Marcin Polak <mapoart@gmail.com>
2020-09-13 00:18:39 +0200

    Added sudo for default install

--- go.linux.nexss.config.js
+++ go.linux.nexss.config.js
@@ -1,11 +1,35 @@
 let languageConfig = Object.assign({}, require("./go.win32.nexss.config"));
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
 languageConfig.compilers = {
   go: {
-    install: "apt install golang",
+    install: `${sudo}apt install golang`,
     command: "go",
     args: "run <file>",
     help: ``,
   },
 };
+
+const {
+  replaceCommandByDist,
+  dist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+const distName = dist();
+languageConfig.dist = distName;
+
+// TODO: Later to cleanup this config file !!
+switch (distName) {
+  case "Arch Linux":
+    languageConfig.compilers.go.install = `${sudo}pacman -Sy --noconfirm go`;
+    break;
+  default:
+    languageConfig.compilers.go.install = replaceCommandByDist(
+      languageConfig.compilers.go.install
+    );
+    break;
+}
 
 module.exports = languageConfig;
